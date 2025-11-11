a361c96b0ea6c730ae1a55728ffc35e992187f9b
sai sarayu <sarayu2926@gmail.com>
2025-11-11 14:01:01 +0530

    done

---
+++ backend/src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("💳 Payment Gateway API running...");
+});
+
+export default app;
